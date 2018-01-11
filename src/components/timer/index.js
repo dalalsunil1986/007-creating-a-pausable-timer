111e9be406aff4dc3c4bc74e0234ec376209e532
Mark Tellez <mark.tellez@icloud.com>
2018-01-11 10:30:08 -0600

    done

--- src/components/timer/index.js
+++ src/components/timer/index.js
@@ -1,100 +1,45 @@
 import React from 'react'
-import formatDuration from 'format-duration'
-
-import ActionButtons from './action-buttons'
-import _elapsedTime from '../../modules/elapsedTime'
+import ElapsedTime from './elapsed-time'
+import Buttons from './buttons'
 
 import './styles.css'
 
 class Timer extends React.Component {
   constructor(props) {
     super(props)
-    const timingEvents = JSON.parse(
-      localStorage.getItem('timingEvents')
-    ) || []
-    const stopped = JSON.parse(localStorage.getItem('stopped')) || false
+
     this.state = {
-      timingEvents: timingEvents.map(date => new Date(date)),
-      stopped,
+      timingEvents: [],
       nonce: 0,
     }
 
-    this.elapsedTime = this.elapsedTime.bind(this)
-    this.startTimer = this.startTimer.bind(this)
-    this.stopTimer = this.stopTimer.bind(this)
-    this.pauseTimer = this.pauseTimer.bind(this)
-    this.addTimingEvent = this.addTimingEvent.bind(this)
-    this.isRunning = this.isRunning.bind(this)
-    this.hasStarted = this.hasStarted.bind(this)
+    this.addTimerEvent = this.addTimerEvent.bind(this)
     this.tick = this.tick.bind(this)
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(this.tick, 1000)
-  }
-  
-  componentWillUnmount() {
-    clearInterval(this.interval)
+    this.poll = setInterval(this.tick, 1000)
   }
 
   tick() {
     this.setState((prevState) => ({ nonce: prevState.nonce+1 }))
   }
 
-  elapsedTime() {
-    return _elapsedTime(this.state.timingEvents)
-  }
-
-  addTimingEvent() {
-    const timingEvents = [...this.state.timingEvents, new Date()]
-    localStorage.setItem('timingEvents',
-      JSON.stringify(timingEvents)
-    )
-    this.setState({timingEvents})
-  }
-
-  startTimer() {
-    this.addTimingEvent()
-  }
-
-  pauseTimer() {
-    if (!this.hasStopped())
-      this.addTimingEvent()
-  }
-
-  stopTimer() {
-    if (this.hasStopped()) return
-    this.addTimingEvent()
-    localStorage.setItem('stopped', JSON.stringify(true))
-    this.setState({ stopped: true })
-  }
-
-  isRunning() {
-    return !this.hasStopped() &&
-           this.state.timingEvents.length % 2
-  }
-
-  hasStarted() {
-    return this.state.timingEvents.length > 0
-  }
-
-  hasStopped() {
-    return this.state.stopped
+  addTimerEvent() {
+    this.setState({
+      timingEvents: [
+        ...this.state.timingEvents,
+        new Date()
+      ]
+    })
   }
 
   render() {
     return (
       <div className='container'>
-        <span>
-          { formatDuration(this.elapsedTime()) }
-        </span>
-        <ActionButtons
-          hasStarted={this.hasStarted()}
-          hasStopped={this.hasStopped()}
-          isRunning={this.isRunning()}
-          startTimer={this.startTimer}
-          pauseTimer={this.pauseTimer}
-          stopTimer={this.stopTimer}
+        <ElapsedTime
+          timingEvents={this.state.timingEvents}
+        />
+        <Buttons
+          handleClick={this.addTimerEvent}
+          timingEvents={this.state.timingEvents}
         />
       </div>
     )
